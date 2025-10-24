99adc763e818789bbcde7785c920061877bf82e3
victoria <victoriabonin@protonmail.com>
2025-10-24 22:37:34 +0200

    fixed temp database data init

--- Infrastructure/Repositories/IngredientsRepository.cs
+++ Infrastructure/Repositories/IngredientsRepository.cs
@@ -7,18 +7,6 @@ public class IngredientsRepository : IIngredientsRepository
 {
     public async Task<List<Ingredient>> GetIngredientsAsync()
     {
-        return new List<Ingredient>()
-        {
-            new Ingredient()
-            {
-                Name = "Rice",
-                Id = 1,
-            },
-            new Ingredient()
-            {
-                Name = "Beans",
-                Id = 2,
-            }
-        };
+        return TempDataBase.Ingredients;
     }
 }
--- Infrastructure/TempDataBase.cs
+++ Infrastructure/TempDataBase.cs
@@ -3,136 +3,124 @@ using Domain.Models;
 
 namespace Infrastructure;
 
-public static class TempDataBase
+public class TempDataBase
 {
-    public static List<Ingredient> Ingredients { get; set; }
-    public static List<Recipe> Recipes { get; set; }
-    public static List<IngredientRecipe> IngredientRecipes { get; set; }
-
-    public static void InitDataBase()
+    public static List<Ingredient> Ingredients { get; set; } = new List<Ingredient>()
     {
-        Ingredients.Add(new Ingredient()
+        new Ingredient()
         {
             Id = 1,
             Name = "Rice",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 2,
             Name = "Beans",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 3,
             Name = "Lettuce",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 4,
             Name = "Tomato",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 5,
             Name = "Chicken",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 6,
             Name = "Arborio rice",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 7,
             Name = "Oyster mushroom",
-        });
-
-        Ingredients.Add(new Ingredient()
+        },
+        new Ingredient()
         {
             Id = 8,
             Name = "Leek",
-        });
+        }
+    };
 
-        Recipes.Add(new Recipe()
+    public static List<Recipe> Recipes { get; set; } = new List<Recipe>()
+    {
+        new Recipe()
         {
             Id = 1,
             Name = "Rice and Beans with protein and salad",
-        });
-
-        Recipes.Add(new Recipe()
+        },
+        new Recipe()
         {
             Id = 2,
             Name = "Leek risotto with oyster mushroom",
-        });
+        }
+    };
 
-        IngredientRecipes.Add(new IngredientRecipe()
+
+    public static List<IngredientRecipe> IngredientRecipes { get; set; } = new List<IngredientRecipe>()
+    {
+        new IngredientRecipe()
         {
             IngredientId = 1,
             RecipeId = 1,
             UnitOfMeasurement = UnitOfMeasurement.Cups,
             Quantity = 0.5
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 2,
             RecipeId = 1,
             UnitOfMeasurement = UnitOfMeasurement.Cups,
             Quantity = 0.5
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 1,
             RecipeId = 3,
             UnitOfMeasurement = UnitOfMeasurement.ToTaste,
             Quantity = 1
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 1,
             RecipeId = 3,
             UnitOfMeasurement = UnitOfMeasurement.Unit,
             Quantity = 1
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 1,
             RecipeId = 5,
             UnitOfMeasurement = UnitOfMeasurement.Grams,
             Quantity = 200
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 6,
             RecipeId = 2,
             UnitOfMeasurement = UnitOfMeasurement.Cups,
             Quantity = 0.5
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 7,
             RecipeId = 2,
             UnitOfMeasurement = UnitOfMeasurement.Unit,
             Quantity = 0.5
-        });
-
-        IngredientRecipes.Add(new IngredientRecipe()
+        },
+        new IngredientRecipe()
         {
             IngredientId = 8,
             RecipeId = 2,
             UnitOfMeasurement = UnitOfMeasurement.Grams,
             Quantity = 100
-        });
-    }
+        }
+    };
 }
